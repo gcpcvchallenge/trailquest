823bab5951c8ac0dd44f6a17763ba3cdfffa8354
gcpcvchallenge <gcpcvchallenge@gmail.com>
2025-08-12 15:40:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: '/trailquest/', // 👈 chemin correct pour GitHub Pages
   plugins: [
     react(),
     runtimeErrorOverlay(),
